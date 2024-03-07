0f7ea082cf80cb983f76792f7f661c1c74019973
Vissarionov.Ivan <Vissarionov.i.u@ya.ru>
2024-03-07 21:05:55 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-          int balans = 100;
-        int creditingMoney = 5550;
+        int balans = 100;
+        int creditingMoney = 5450;
         if (creditingMoney >= 1001) {
 
             int bonus = creditingMoney / 100;
             System.out.println(balans + creditingMoney + bonus);
 
-        } else {
-            System.out.println(balans + creditingMoney);
-        }
-//
-//        int balans = 100;
-//        int creditingMoney = 1050;
-//        boolean bonusСondition = true;
-//        if (creditingMoney >= 1001) {
-//            int percent = bonusСondition ? 1 : 0;
-//            int bonus = creditingMoney / 100 * percent;
-//            System.out.println(balans + creditingMoney + bonus);
-//
-//        } else {
-//            System.out.println(balans + creditingMoney);
-//        }
-//        int percent;
-//        if (iSregistered) {
-//            percent = 1;
-//        } else {
-//            percent = 0;
-//        }
+        } else System.out.println(balans + creditingMoney);
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
